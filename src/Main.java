40d893c839c401e6cb095492780b96aeec89b5be
Alex Deyanov <deyanovan@yandex.ru>
2023-12-21 20:01:41 +0300

    ReformatCode

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int amount = 18_659; // потраченная суммма
         int mile = 20; // сумма для начисления одной бонусной мили
 
-        int bonus = amount/mile;
+        int bonus = amount / mile;
         System.out.println(bonus + " бонусных миль начисленно");
     }
 }
